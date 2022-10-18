cfc0942e9a9bd487d570457b042ba3846bd046ae
Juan Carlos Afonso <jafonso@grafcan.com>
2022-10-18 13:55:13 +0100

    Camel case

--- life-viewer/main.js
+++ life-viewer/main.js
@@ -1,8 +1,8 @@
-import {init_map} from './scripts.js';
+import {initMap} from './scripts.js';
 import './style.css';
 
 fetch('/data/layers.json')
   .then((response) => response.json())
   .then((data) => {
-    init_map(data);
+    initMap(data);
   });
