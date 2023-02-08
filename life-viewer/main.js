d06c6a0c4d0d73737b7e19230cf45e587e0a97f4
Juan Carlos Afonso <jafonso@grafcan.com>
2023-02-08 12:11:35 +0000

    Uso de JSON de contenidos según modo - development: layers.json - preproduction: layers.json - production: layers_prod.json

--- life-viewer/main.js
+++ life-viewer/main.js
@@ -1,7 +1,13 @@
 import {initMap} from './modules/scripts';
 import './style.css';
 
-fetch('/data/layers.json')
+let data_url
+if (import.meta.env.MODE == 'production') {
+  data_url = '/data/layers_prod.json'
+} else {
+  data_url = '/data/layers.json'
+}
+fetch(data_url)
   .then((response) => response.json())
   .then((data) => {
     initMap(data);
