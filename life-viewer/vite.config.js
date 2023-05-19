1a17e77a9e1e8e29b431f022bd3e75b20d466768
Juan Carlos Afonso <jafonso@grafcan.com>
2023-05-19 10:01:01 +0100

    vite-plugin-radar para GA4

--- life-viewer/vite.config.js
+++ life-viewer/vite.config.js
@@ -1,5 +1,16 @@
+import { VitePluginRadar } from 'vite-plugin-radar'
+
 export default {
   build: {
     sourcemap: true,
-  }
+  },
+  plugins: [
+    VitePluginRadar({
+      // Google Analytics tag injection
+      enableDev: true,
+      analytics: {
+        id: 'G-VVQPW150PB',
+      },
+    })
+  ],
 }
